1a5698fd4b10fa62853489ab8331f31fed00fdc4
Alexander Gude <alex.public.account@gmail.com>
2013-08-22 16:10:39 -0500

    Update indenting in index.php HTML
    The HTML generated by index.php is now indented 2 spaces for every level
    of tag.

--- webpages/index.php
+++ webpages/index.php
@@ -11,31 +11,31 @@ natcasesort($files);
                                                                                                   
 // Opening HTML
 echo "<html>\n";
-echo "<head>\n";
-echo "<meta http-equiv=\"cache-control\" content=\"max-age=0\" />\n";
-echo "<meta http-equiv=\"cache-control\" content=\"no-cache\" />\n";
-echo "<meta http-equiv=\"expires\" content=\"0\" />\n";
-echo "<meta http-equiv=\"expires\" content=\"Tue, 01 Jan 1980 1:00:00 GMT\" />\n";
-echo "<meta http-equiv=\"pragma\" content=\"no-cache\" />\n";
-echo "<meta name=\"robots\" content=\"noindex\">\n <!-- Prevent indexing -->\n";
-echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
-echo "</head>\n";
-echo "<body>\n";
-echo "<div class=\"content\">\n";
+echo "  <head>\n";
+echo "    <meta http-equiv=\"cache-control\" content=\"max-age=0\" />\n";
+echo "    <meta http-equiv=\"cache-control\" content=\"no-cache\" />\n";
+echo "    <meta http-equiv=\"expires\" content=\"0\" />\n";
+echo "    <meta http-equiv=\"expires\" content=\"Tue, 01 Jan 1980 1:00:00 GMT\" />\n";
+echo "    <meta http-equiv=\"pragma\" content=\"no-cache\" />\n";
+echo "    <meta name=\"robots\" content=\"noindex\">\n <!-- Prevent indexing -->\n";
+echo "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
+echo "  </head>\n";
+echo "  <body>\n";
+echo "    <div class=\"content\">\n";
 
 // Display images 
 foreach($files as $file) {
         $ext = end(explode('.', $file));
         if($ext == "jpg" || $ext == "png") {
-            echo "<hr><br>\n";
-            echo "<h1>$file</h1><br>\n";
-            echo "<img src=\"$file\" alt=\"Image\" /><br>\n";
+            echo "      <hr><br>\n";
+            echo "      <h1>$file</h1><br>\n";
+            echo "      <img src=\"$file\" alt=\"Image\" /><br>\n";
         }
 }
 
 // Final line and closing HTML
-echo "<hr>\n";
-echo "</div>\n";
-echo "</body>\n";
+echo "      <hr>\n";
+echo "    </div>\n";
+echo "  </body>\n";
 echo "</html>\n";
 ?>
